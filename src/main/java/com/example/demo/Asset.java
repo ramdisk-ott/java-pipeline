17a231ed8067dc21312967a9d1a3d958d4a33234
Ramesh M <62267259+ramdisk-ott@users.noreply.github.com>
2022-10-09 20:49:04 +0530

    list asset added

---
+++ src/main/java/com/example/demo/Asset.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class Asset {
+    private String  ID;
+    private String  Owner;
+    private String  Size;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public String getSize() {
+        return Size;
+    }
+
+    public void setSize(String size) {
+        Size = size;
+    }
+}
--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,5 +14,18 @@ public class SampleController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String sample() {
         return "test api  is working";
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public List<Asset> list() {
+        List<Asset> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Asset asset = new Asset();
+            asset.setID((i + 1) + "");
+            asset.setSize((1) + "");
+            asset.setOwner("Owner-" + (i + 1));
+            list.add(asset);
+        }
+        return list;
     }
 }
